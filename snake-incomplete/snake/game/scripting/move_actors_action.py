32c97e75eedb180ecc13625334acc69100e4b46e
adaobi okudo <oku22001@byui.edu>
2022-10-16 22:17:21 -0700

    commit

--- snake-incomplete/snake/game/scripting/move_actors_action.py
+++ snake-incomplete/snake/game/scripting/move_actors_action.py
@@ -7,3 +7,38 @@ from game.scripting.action import Action
 # 1) get all the actors from the cast
 # 2) loop through the actors
 # 3) call the move_next() method on each actor
+class MoveActorsAction(Action):
+
+    """An action that causes the actor to move. 
+
+    
+
+    The responsibility of MoveActorsAction is to move each actor in the cast to their next location
+
+
+
+    Attributes:
+
+        _actors (list): actors in the cast
+
+    """
+
+
+
+    def __init__(self):
+
+        super().__init__()
+
+        self._actors = []
+
+
+
+    def execute(self, cast, script):
+
+        self._actors = cast.get_all_actors()
+
+        for _actor in self._actors:
+
+
+
+            _actor.move_next()
